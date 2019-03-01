51011c58828a226d15b6608974d5e7814d40de76
Dimitry Volker <yerath@Dimitrys-Air.fritz.box>
2019-03-01 23:58:45 +0100

    Init

---
+++ FSharpFFAP/FSharpFFAP/SumOfSquares.fsx
@@ -0,0 +1,9 @@
+﻿
+//Define Square function
+let square x = x * x
+
+//define the sumOfSquare function
+let sumOfSquares n = 
+    [1..n] |> List.map square |> List.sum
+
+let answer = sumOfSquares 100
